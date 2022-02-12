88a551c055cab36f5063e9d3230325849af199b6
whiteboard <thewhiteboard2019@gmail.com>
2022-02-12 18:54:16 +0530

    day3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/home_page.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:flutter_application_1/pages/login_page.dart';
 void main(){
   runApp(MyApp());
 }
@@ -10,7 +12,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return MaterialApp(
-      home: HomePage()
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.blueGrey ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/" : (context) => LoginPage(),
+        "/login": (context) => LoginPage(),
+        "/home": (context) =>HomePage(),
+      },
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,7 +7,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
      return  Scaffold(
        appBar:AppBar(
-         title: Text("Catelog App"),
+         title: Text("Catalog App"),
        ),
        body:Material(
           child: Center(
